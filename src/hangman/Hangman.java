7d4ac5a2d5f9435ffc3bb4044018dadbb160a256
Wes <wredmond@squarespace.com>
2017-07-10 14:06:22 +0100

    first commit

---
+++ src/hangman/Hangman.java
@@ -0,0 +1,22 @@
+
+package hangman;
+
+
+//@author wredmond
+
+public class Hangman {
+
+    public static void main(String[] args) {
+        
+        Game game = new Game("treehouse");
+        Prompter prompter = new Prompter(game);
+        
+        while (game.getRemainingTries() > 0) {
+            prompter.displayProgress();
+            prompter.promptForGuess();
+        }
+        
+        
+    }
+
+}
